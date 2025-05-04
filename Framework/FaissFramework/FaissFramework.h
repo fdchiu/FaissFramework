6dad73ae04e50bcce6a6329ad3771b51d878e336
David Chiu <fdchiu@gmail.com>
2025-05-04 19:40:20 +0800

    first commit

---
+++ Framework/FaissFramework/FaissFramework.h
@@ -0,0 +1,18 @@
+//
+//  FaissFramework.h
+//  FaissFramework
+//
+//  Created by david Chiu on 11/21/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaissFramework.
+FOUNDATION_EXPORT double FaissFrameworkVersionNumber;
+
+//! Project version string for FaissFramework.
+FOUNDATION_EXPORT const unsigned char FaissFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaissFramework/PublicHeader.h>
+
+
